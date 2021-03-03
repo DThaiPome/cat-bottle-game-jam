3fc58fa07739f3008afe249da15a46637270ba1b
DThaiPome <r.gehrlein154@gmail.com>
2021-03-03 16:19:37 -0500

    Cat collider properly changes when looking in a direction

--- Cat Bottle RIP Game Jam/Assets/Scripts/Player/PlayerShape.cs
+++ Cat Bottle RIP Game Jam/Assets/Scripts/Player/PlayerShape.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(IPlayerStateMachine))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerShape : MonoBehaviour
 {
     private IPlayerStateMachine states;
+    private BoxCollider2D collider;
+    private Vector2 baseSize;
+    private Vector2 baseOffset;
 
     void Start()
     {
         this.states = this.GetComponent<IPlayerStateMachine>();
         this.states.OnStateEnter(this.OnEnterLooking, PlayerState.Looking);
         this.states.OnStateStart(this.OnEnterLooking, PlayerState.Looking);
+        this.states.OnStateEnter(this.OnEnterStanding, PlayerState.Standing);
+        this.states.OnStateStart(this.OnEnterStanding, PlayerState.Standing);
+
+        this.collider = this.GetComponent<BoxCollider2D>();
+        this.baseSize = this.collider.size;
+        this.baseOffset = this.collider.offset;
     }
     
     void OnEnterLooking()
@@ -23,17 +34,26 @@ public class PlayerShape : MonoBehaviour
             angle = 0;
         } else if (direction.Equals(Vector2.up))
         {
-            angle = 45;
+            angle = 90;
         } else if (direction.Equals(Vector2.left))
         {
-            angle = 90;
+            angle = 180;
         } else if (direction.Equals(Vector2.down))
         {
-            angle = 135;
+            angle = 270;
         } else
         {
             angle = 0;
         }
         this.transform.eulerAngles = Vector3.forward * angle;
+
+        this.collider.size = new Vector2(this.baseSize.x * 2, this.baseSize.y);
+        this.collider.offset += Vector2.right * 0.5f;
+    }
+
+    void OnEnterStanding()
+    {
+        this.collider.size = this.baseSize;
+        this.collider.offset = this.baseOffset;
     }
 }
