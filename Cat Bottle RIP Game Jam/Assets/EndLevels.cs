8eac8831fb77128ebd85fc818308916b0636773c
lennieok <elena.o.kosowski@gmail.com>
2021-03-07 15:59:17 -0500

    added end of Level with reset and exit

---
+++ Cat Bottle RIP Game Jam/Assets/EndLevels.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class EndLevels : MonoBehaviour
+{
+
+    public void ResetGame(){
+
+        Debug.Log("You have clicked to reset");
+         SceneManager.LoadScene("Level1");
+
+
+    }
+
+    public void EndGame(){
+
+         Debug.Log("You have clicked to end");
+         Application.Quit();
+        
+
+    }
+
+
+}
