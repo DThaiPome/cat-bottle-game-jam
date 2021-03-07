30689e41ab90cac657242313f13894f63a4b4c2d
DThaiPome <r.gehrlein154@gmail.com>
2021-03-07 16:02:09 -0500

    TV visually breaks

--- Cat Bottle RIP Game Jam/Assets/Scripts/TVBehavior.cs
+++ Cat Bottle RIP Game Jam/Assets/Scripts/TVBehavior.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class TVBehavior : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite normalTV;
+    [SerializeField]
+    private Sprite brokenTV;
+
     public bool broken = false;
     public LevelManager levelM;
+
+    private SpriteRenderer ren;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.ren = this.GetComponent<SpriteRenderer>();
+
         this.levelM = GameObject.Find("LevelManager").GetComponent<LevelManager>();
     }
 
@@ -19,6 +29,14 @@ public class TVBehavior : MonoBehaviour
         if (broken && !this.gameOver())
         {
             levelM.LevelBeat();
+        }
+
+        if (broken)
+        {
+            this.ren.sprite = this.brokenTV;
+        } else
+        {
+            this.ren.sprite = this.normalTV;
         }
     }
 
